70361344fc5de55298615bb44418c46f19df089e
David Bell <dave@evad.info>
2016-11-04 14:35:46 +0000

    added SSH keypair generation to api register

--- zero/views/api.py
+++ zero/views/api.py
@@ -4,6 +4,13 @@ from zero import app
 from zero.lib.user import is_logged_in, authenticate, is_user_in_group
 from flask import Flask, request, session, redirect, url_for, flash, g, abort, make_response, render_template, jsonify
 import MySQLdb as mysql
+import re
+
+####
+from paramiko import RSAKey
+####
+import StringIO
+####
 
 ################################################################################
 
@@ -12,35 +19,64 @@ import MySQLdb as mysql
 def api_register():
 	"""Simple REST API endpoint to allow workstations to register"""
 
+	app.logger.debug("call: api_register()")
+
+	## We return 200 OK whatever errors we encounter
+	## we return JSON, and if there is an error
+	## the JSON has an 'error' variable set with the problem
+
 	username = request.form['username']
 	#os_ident = request.form['ident']
-	#hostname = request.form['hostname']
 	#metadata = request.form['metadata']
 	#facts    = request.form['facts']
+
+	app.logger.debug("api_register: username sent was " + username)
 
 	## Check username + password in LDAP
 	result = authenticate(username, request.form['password'])
 
 	if not result:
-		app.logger.debug("New registration request failed due to authentication failure for " + username)
-		abort(403)
+		app.logger.debug("api_register: request failed, authentication failure for " + username)
+		return jsonify({'error': True, 'reason': "Authentication failure, incorrect username and/or password"})
 	else:
-		app.logger.info("New registration request from authenticated user " + username)
+		app.logger.debug("api_register: authentication success for " + username)
 
 	## Only allow iSolutions staff to register systems
 	if not is_user_in_group(username,"jfStaff"):
-		abort(406)
+		app.logger.debug("api_register: request failed, the user " + username + " is not in jfStaff")
+		return jsonify({'error': True, 'reason': "Permission denied, you must be a member of iSolutions staff"})
 
-	## Validate hostname if sent
-	if 'hostname' in request.form:
-		pass
-	else:
-		## Generate a hostname
-		pass
+	## Validate hostname
+	hostname = request.form['hostname']
 
-	## Validate metadata
+	if not re.match(r"^(uos|iss|lnx)\-[0-9]{2,8}$",hostname):
+		app.logger.debug("api_register: request failed, the hostname " + hostname + " is invalid")
+		return jsonify({'error': True, 'reason': "The hostname is invalid, it must be the uos-number of the system"})
 
-	## Check if the system was already registered
+	## TODO Validate metadata
 
-	return jsonify(['OK'])
+	## TODO check facts
+
+	## TODO Check if the system was already registered
+
+	## Generate an SSH RSA private key
+	private_key = RSAKey.generate(bits=2048)
+	fakefile    = StringIO.StringIO()
+	private_key.write_private_key(fakefile)
+
+	## Reseek to 0 in our fake StringIO file
+	fakefile.seek(0)
+
+	## Generate an SSH RSA public key
+	public_key = RSAKey.from_private_key(fakefile)
+
+	## Turn it into a more usual output format
+	public_key_str = public_key.get_name() + " " + public_key.get_base64()
+
+	app.logger.debug("api_register: registration succeeded for " + hostname + " using account " + username)
+	return jsonify({'private_key': fakefile.getvalue(), 'public_key': public_key_str})
+
+
+
+
 
