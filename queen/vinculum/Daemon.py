b9d5f969fb35bbf08b6f1e3086949cff9de25465
David Bell <dave@evad.info>
2017-01-06 14:28:06 +0000

    commiting broken code

--- queen/vinculum/Daemon.py
+++ queen/vinculum/Daemon.py
@@ -9,8 +9,9 @@ import json
 from setproctitle import setproctitle #pip install setproctitle
 import subprocess
 import yum
+import multiprocessing
+from multiprocessing import Process, Queue
 
-# we use multiplex, rather than threading, cos the GIL sucks.
 Pyro4.config.SERVERTYPE = "multiplex"
 Pyro4.config.SOCK_REUSE = True
 
@@ -35,33 +36,54 @@ def set_socket_permissions(socket_path):
 
 class VinculumDaemon(object):
 
-	pyro  = None
-
 	## PRIVATE METHODS #########################################################
 
 	def __init__(self, pyro):
 		syslog.openlog("vinculum", syslog.LOG_PID)
 
 		## rename the process title
-		setproctitle("vinculum")
+		setproctitle("vinculum-master")
 
 		## Store the copy of the pyro daemon object
 		self.pyro = pyro
 
 		## Set up signal handlers
-		signal.signal(signal.SIGTERM, self._signal_handler_term)
-		signal.signal(signal.SIGINT, self._signal_handler_int)
+		signal.signal(signal.SIGTERM, self._signal_handler_master)
+		signal.signal(signal.SIGINT, self._signal_handler_master)
 
-	def _signal_handler_term(self, signal, frame):
-		self._signal_handler('SIGTERM')
-	
-	def _signal_handler_int(self, signal, frame):
-		self._signal_handler('SIGINT')
-	
-	def _signal_handler(self, signal):
-		syslog.syslog('caught ' + str(signal) + "; exiting")
+		## Set up the queue of tasks for packages to install/remove
+		self.pkgTaskQueue = Queue()
+
+		## Start the process for performing package installs
+		pkgProcess = Process(target=self._pkgProcess, args=(self.pkgTaskQueue,))
+		pkgProcess.start()
+
+		syslog.syslog('vinculum-master started')
+
+	def _signal_handler_master(self, sig, frame):
+		if sig == signal.SIGTERM: 
+			sig = "SIGTERM"
+		elif sig == signal.SIGINT:
+			sig = "SIGINT"
+
+		syslog.syslog('master process caught ' + str(sig) + "; exiting")
 		Pyro4.core.Daemon.shutdown(self.pyro)
 		sys.exit(0)
+
+	def _signal_handler_child(self, sig, frame):
+		if sig == signal.SIGTERM: 
+			sig = "SIGTERM"
+		elif sig == signal.SIGINT:
+			sig = "SIGINT"
+
+		syslog.syslog('child process caught ' + str(sig) + "; exiting")
+		sys.exit(0)
+
+	## This is called on each pyro loop timeout/run to make sure defunct processes
+	## (finished tasks waiting for us to reap them) are reaped
+	def _onloop(self):
+		multiprocessing.active_children()
+		return True
 
 	def sysexec(self,command,shell=False,env={}):
 		try:
@@ -77,20 +99,66 @@ class VinculumDaemon(object):
 			return (proc.returncode,str(stdoutdata))
 		except Exception as ex:
 			return (1,str(type(ex)) + " " + str(ex))
+
+	def _pkgProcess(self,q):
+		setproctitle("vinculum-pkg")
+		syslog.openlog("vinculum-pkg", syslog.LOG_PID)
+		signal.signal(signal.SIGTERM, self._signal_handler_child)
+		signal.signal(signal.SIGINT, self._signal_handler_child)
+		syslog.syslog('vinculum-pkg started')
+	
+		while True:
+			task = q.get(block=True)
+			syslog.syslog("package task obtained")
+
+			if task['task'] == 'pkgInstall':
+				pkg_names = task['data']
+
+				try:
+					yb=yum.YumBase()
+					yb.conf.cache = 0
+					installedPkgs = yb.rpmdb.returnPackages()
+					installedPkgNames=[x.name for x in installedPkgs]
+
+					found_any = False
+					for pkg in pkg_names:
+						if pkg in installedPkgNames:
+							syslog.syslog("package " + pkg + " is already installed, skipping")
+							continue
+
+						searchlist=['name']
+						arg=[pkg]
+						matches = yb.searchGenerator(searchlist,arg)
+
+						found = False
+						for (package, matched_value) in matches:
+							if package.name == pkg:
+								syslog.syslog("marking " + package.name + " for installation " + str(package))
+								yb.install(package)
+								found_any = True
+								found = True
+
+						if not found:
+							syslog.syslog("package " + pkg + " could not be found, skipping")
+
+					if found_any:
+						syslog.syslog("running transaction check")
+						yb.buildTransaction()
+						syslog.syslog("installing package(s)")
+						yb.processTransaction()
+						syslog.syslog("package installation complete")
+					else:
+						syslog.syslog("could not find any valid package names to install")
+
+				except Exception as ex:
+					syslog.syslog("Error during package install: " + str(type(ex)) + " " + str(ex))				
 		
 	## RPC METHODS #############################################################
 
-	## pkgInstall
-	## pkgRemove
-	## pkgGroupInstall
-	## pkgGroupRemove
 	## groupAddUser
 	## groupRemoveUser
 	## 
 	## backupNow
-	## policySync
-	## policyApply
-	## policyRun
 
 	@Pyro4.expose
 	def ping(self):
@@ -98,30 +166,19 @@ class VinculumDaemon(object):
 
 	@Pyro4.expose
 	def pkgInstall(self,pkg_names):
-		yb=yum.YumBase()
-		yb.conf.cache = 0
-		installedPkgs = yb.rpmdb.returnPackages()
-		installedPkgNames=[x.name for x in installedPkgs]
+		self.pkgTaskQueue.put({'task': 'pkgInstall', 'data': pkg_names})
+		syslog.syslog("added package install task to queue")
 
-		found = False
-		for pkg in pkg_names:
-			if pkg in installedPkgNames:
-				raise Exception("Package '" + pkg + "' is already installed")
+	def pkgRemove(self,pkg_names):
+		self.pkgTaskQueue.put({'task': 'pkgRemove', 'data': pkg_names})
+		syslog.syslog("added package remove task to queue")
 
-			searchlist=['name']
-			arg=[pkg]
-			matches = yb.searchGenerator(searchlist,arg)
+	@Pyro4.expose
+	def pkgGroupInstall(self,grp_names):
+		self.pkgTaskQueue.put({'task': 'pkgGroupInstall', 'data': grp_names})
 
-			for (package, matched_value) in matches:
-				if package.name == pkg:
-					yb.install(package)
-					found = True
-	
-		if found:
-			yb.buildTransaction()
-			yb.processTransaction()
-		else:
-			raise Exception("Could not find matching packages to install")
+	def pkgGroupRemove(self,grp_names):
+		self.pkgTaskQueue.put({'task': 'pkgGroupRemove', 'data': grp_names})
 
 	@Pyro4.expose
 	def request_backup(self,name):
