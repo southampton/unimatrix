932026f4225b0f7e6c1f1f8e3f4d25c018f3364b
David Bell <dave@evad.info>
2017-01-24 16:31:34 +0000

    updated to work in the installed env

--- deskctl/views/main.py
+++ deskctl/views/main.py
@@ -17,9 +17,10 @@ def default():
 
 @app.route('/software')
 def software():
-
+	## Connect to the desktop management service
 	deskctld = deskctld_connect()
 
+	## Get the current pkg task queue
 	pkgStatus = deskctld.pkgStatus()
 
 	if len(pkgStatus) == 0:
@@ -31,6 +32,9 @@ def software():
 def permissions(group):
 	if group not in ['users','vboxusers','sys','wheel']:
 		abort(404)
+
+	## Connect to the desktop management service
+	deskctld = deskctld_connect()
 
 	## Get current group members
 	try:
@@ -60,21 +64,21 @@ def permissions(group):
 				elif session['username'] in localsys.gr_mem:
 					can_make_changes = True
 
-	if request.method == 'GET':
-		## explanation text
-		if group == "users":
-			group_title = "SSH Access"
-			group_desc  = "The users listed below can logon to this system remotely via SSH"
-		elif group == "vboxusers":
-			group_title = "VirtualBox Users"
-			group_desc  = "The users listed below can create and run virtual machines within VirtualBox"
-		elif group == "sys":
-			group_title = "Administrators"
-			group_desc  = "The users listed below are granted semi-administrator access, including software management, and detailed settings management. Most users, especially laptop users, should be listed here."
-		elif group == "wheel":
-			group_title = "Root Access"
-			group_desc  = "The users listed below are granted FULL root access. Please use this sparingly. If a user is listed below iSolutions standard support ENDS!"
+	## explanation text
+	if group == "users":
+		group_title = "SSH Access"
+		group_desc  = "The users listed below can logon to this system remotely via SSH"
+	elif group == "vboxusers":
+		group_title = "VirtualBox Users"
+		group_desc  = "The users listed below can create and run virtual machines within VirtualBox"
+	elif group == "sys":
+		group_title = "Administrators"
+		group_desc  = "The users listed below are granted semi-administrator access, including software management, and detailed settings management. Most users, especially laptop users, should be listed here."
+	elif group == "wheel":
+		group_title = "Root Access"
+		group_desc  = "The users listed below are granted FULL root access. Please use this sparingly. If a user is listed below iSolutions standard support ENDS!"
 
+	if request.method == 'GET':
 		group_members = []
 		for member in grmembers.gr_mem:
 	
@@ -82,7 +86,7 @@ def permissions(group):
 				pwentry = pwd.getpwnam(member)
 				gecos = pwentry.pw_gecos
 			except KeyError as ex:
-				gecos = "N/A - account deleted?"
+				gecos = "N/A (user no longer exists)"
 
 			group_members.append({'username': member, 'gecos': gecos})
 
@@ -91,3 +95,33 @@ def permissions(group):
 	elif request.method == 'POST':
 		if not can_make_changes:
 			abort(403)
+
+		if 'action' not in request.form:
+			abort(400)
+
+		if request.form['action'] == 'add':
+			username = request.form['username']
+
+			try:
+				pwentry = pwd.getpwnam(username)
+			except KeyError as ex:
+				flash("The username you specified is invalid","alert-danger")
+				return redirect(url_for('permissions',group=group))
+
+			## Try adding them to the group
+			try:
+				deskctld.groupAddUser(group,username)
+			except Exception as ex:
+				flash("Could not add username to group: " + str(ex))
+				return redirect(url_for('permissions',group=group))
+
+			flash("Added '" + username + "' to " + group_title)
+			return redirect(url_for('permissions',group=group))
+
+		elif request.form['action'] == 'remove':
+			# stuff	
+			pass
+
+
+
+
