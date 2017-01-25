d459bd1a767af3a127f93d31c5eae114ab2342e6
David Bell <dave@evad.info>
2017-01-25 16:28:02 +0000

    small bug fix to table create

--- zero/app.py
+++ zero/app.py
@@ -293,7 +293,7 @@ Username:             %s
 		cursor.execute("""CREATE TABLE IF NOT EXISTS `pkg_categories` (
 		  `id` mediumint(11) NOT NULL AUTO_INCREMENT,
 		  `name` varchar(255) NOT NULL,
-		  PRIMARY KEY (`id`),
+		  PRIMARY KEY (`id`)
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
 
 		cursor.execute("""CREATE TABLE IF NOT EXISTS `pkg_entries` (
