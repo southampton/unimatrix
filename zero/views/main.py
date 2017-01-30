9a6deab42182c687e47ee5792201c3245f0e1569
David Bell <dave@evad.info>
2017-01-30 12:45:00 +0000

    added category sorting

--- zero/views/main.py
+++ zero/views/main.py
@@ -3,8 +3,10 @@
 from zero import app
 import zero.lib.user
 from zero.lib.user import is_logged_in
+from zero.lib.plexus import plexus_connect
 from flask import Flask, request, session, redirect, url_for, flash, g, abort, make_response, render_template, jsonify
 import MySQLdb as mysql
+import json
 
 ################################################################################
 
@@ -19,10 +21,42 @@ def default():
 
 
 @zero.lib.user.login_required
-@app.route('/pkgdb/categories')
+@app.route('/pkgdb/categories',methods=['GET','POST'])
 def pkgdb_categories():
 	curd = g.db.cursor(mysql.cursors.DictCursor)
-	curd.execute("SELECT * FROM `pkg_categories` ORDER BY `order`")
-	categories = curd.fetchall()
 
-	return render_template('pkgdb/categories.html',active="pkgdb_categories",categories=categories)
+	if request.method == 'GET':
+		curd.execute("SELECT * FROM `pkg_categories` ORDER BY `order`")
+		categories = curd.fetchall()
+
+		return render_template('pkgdb/categories.html',active="pkgdb_categories",categories=categories)
+
+	elif request.method == 'POST':
+		action = request.form['action']
+
+		if action == 'update':
+			# new order is a json string of an array
+			neworder = request.form['neworder']
+	
+			try:
+				newOrderList = json.loads(neworder)
+			except Exception as ex:
+				abort(400)
+
+			order = 1
+			for catid in newOrderList:
+				curd.execute("UPDATE `pkg_categories` SET `order` = %s WHERE `id` = %s",(order,int(catid)))
+				order = order + 1
+
+			g.db.commit()
+
+			# regenerate the pkgdb with the new order
+			plexus = plexus_connect()
+			plexus.regenerate_pkgdb()
+
+			return "", 200
+
+		elif action == 'new':
+			abort(400)
+		else:
+			abort(400)
