0d40179b6c4c37d1aed7bbda63c35487de36c593
David Bell <dave@evad.info>
2017-01-30 11:35:20 +0000

    switched to patternfly for zero web app, started category/package management

--- zero/views/main.py
+++ zero/views/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from zero import app
+import zero.lib.user
 from zero.lib.user import is_logged_in
 from flask import Flask, request, session, redirect, url_for, flash, g, abort, make_response, render_template, jsonify
 import MySQLdb as mysql
@@ -12,6 +13,16 @@ def default():
 	"""Renders the about page"""
 
 	if is_logged_in():
-		return render_template('error.html', title='welcome', message="You are currently logged in as " + session['username'])
+		return render_template('default.html')
 	else:
 		return redirect(url_for('login'))
+
+
+@zero.lib.user.login_required
+@app.route('/pkgdb/categories')
+def pkgdb_categories():
+	curd = g.db.cursor(mysql.cursors.DictCursor)
+	curd.execute("SELECT * FROM `pkg_categories` ORDER BY `order`")
+	categories = curd.fetchall()
+
+	return render_template('pkgdb/categories.html',active="pkgdb_categories",categories=categories)
