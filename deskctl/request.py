c3a37a721e8b32701aeab74b99951adddf4f34c5
David Bell <dave@evad.info>
2017-02-02 12:59:27 +0000

    removed test script, added test for vboxusers

--- deskctl/request.py
+++ deskctl/request.py
@@ -3,12 +3,21 @@ from deskctl.lib.errors import logerr, fatalerr
 from deskctl.lib.user import is_logged_in
 from flask import Flask, request, session, g, abort, render_template, url_for
 import platform
+import grp
 
 ################################################################################
 
 @app.context_processor
 def context_processor():
+	vboxexists = False
+	try:
+		grp.getgrnam("vboxusers")
+		vboxexists = True
+	except KeyError as ex:
+		pass
+
 	return {
 		"is_logged_in": is_logged_in,
 		"hostname": platform.node().upper(),
+		"vboxexists": vboxexists,
 	}
